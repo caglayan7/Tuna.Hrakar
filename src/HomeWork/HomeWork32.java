170318bf8116cc03baa023d5d894258782084f8d
unknown <hrakaralkhidher@icloud.com>
2022-11-03 13:01:46 -0500

    second commit

--- src/HomeWork/HomeWork32.java
+++ src/HomeWork/HomeWork32.java
@@ -1,5 +1,6 @@
 package HomeWork;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HomeWork32 {
@@ -8,32 +9,6 @@ public class HomeWork32 {
         //1) user should be prompted about how many numbers is going to be entered
         //2) User should be prompted for the values of the numbers
         //3) Reverse the all the given numbers and print it
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter the number ");
-        int number = scan.nextInt();
-        int[] array= new int[number];
 
-        for (int i = 0; i < number; i++) {
-            System.out.println("Please enter the "+(i+1)+". number: ");
-            int num = scan.nextInt();
-            array[i] = num;
-        }
-
-        int sum=0;
-        for (int i = 0; i <array.length ; i++) {
-
-            sum=sum+array[i];
-
-        }
-
-        System.out.println("the sum of all the number in the array is: "+sum);
-        String reversed1 = " ";
-        for (int l = array.length; l > 0 ; l--) {
-
-            reversed1= String.valueOf(reversed1.charAt(l));
-            l++;
-
-        }
-        System.out.println(" the value of the reversed: "+reversed1);
     }
 }
--- src/HomeWork/HomeWork34.java
+++ src/HomeWork/HomeWork34.java
@@ -1,8 +1,23 @@
 package HomeWork;
 
+import java.util.Arrays;
+
 public class HomeWork34 {
     public static void main(String[] args) {
         //Write code where it shifts all the elements in array by one index and put the the first index at the end of the array
         //example array = {2,3,4,5,6,7,8,9} ===> shifted array = {3,4,5,6,7,8,9,2}
+
+        int nums[]  = {2,3,4,5,6,7,8,9};
+        System.out.println("Our Elements before getting shifted "+Arrays.toString(nums));
+        for (int i = 0; i < nums.length-1; i++) {
+        }
+        int first = nums[0];
+        for (int j = 0; j < nums.length-1; j++)
+        {
+        nums[j] = nums[j+1];
+        }
+        nums[nums.length-1]=first;
+            System.out.println("The shifted Arrays:"+Arrays.toString(nums));
+        }
     }
-}
+
