d9e796a15f444ee973e1da68bd7ffea5f8df0b85
unknown <hrakaralkhidher@icloud.com>
2022-11-13 17:00:40 -0600

    first commit

---
+++ src/HomeWork/HomeWork35.java
@@ -0,0 +1,10 @@
+package HomeWork;
+
+public class HomeWork35 {
+    public static void main(String[] args) {
+//Write a method where it takes 2 strings as
+// a parameter and returns true when 2 strings are equal and returns false when they are not equal.
+    }
+
+
+}
---
+++ src/HomeWork/HomeWork36.java
@@ -0,0 +1,8 @@
+package HomeWork;
+
+public class HomeWork36 {
+    public static void main(String[] args) {
+        //Write a method where it takes array of integers and returns the sorted version of that array
+
+    }
+}
