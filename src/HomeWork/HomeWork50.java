2311091d3c037dfe94a2314eda5f89c5f7ab0503
LENOVO <hrakaralkhidher@icloud.com>
2022-12-01 15:37:59 -0600

    just commit

---
+++ src/HomeWork/HomeWork50.java
@@ -0,0 +1,7 @@
+package HomeWork;
+
+public class HomeWork50 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/HomeWork/HomeWork54.java
@@ -0,0 +1,7 @@
+package HomeWork;
+
+public class HomeWork54 {
+    public static void main(String[] args) {
+
+    }
+}
